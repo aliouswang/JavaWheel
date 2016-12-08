d571c1057e1457307cce0adbec7dfed56e807fb7
aliouswang <wsw622@126.com>
2016-12-08 14:34:45 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        String s1 = "pool";
+        String s2 = "pool";
+        String s3 = "po" + "ol";
+
+        System.out.println(s2 == s3);
+
+    }
+}
